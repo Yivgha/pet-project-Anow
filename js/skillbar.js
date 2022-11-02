7361b81ef9da32c193d1d9407b8205d590dfc781
Yevheniia P <90108689+Yivgha@users.noreply.github.com>
2022-11-02 03:17:16 +0200

    1

--- js/skillbar.js
+++ js/skillbar.js
@@ -1,13 +1,19 @@
-function move() {
-  const elem = document.getElementById("progressbar");
-  const width = 1;
-  const id = setInterval(frame, 10);
-  function frame() {
-    if (width >= 100) {
-      clearInterval(id);
-    } else {
-      width++;
-      elem.style.width = width + '%';
+let i = 0;
+    function move() {
+        if (i === 0) {
+            i = 1;
+            const elem = document.getElementById("progressbar");
+            const width = 10;
+            const id = setInterval(frame, 10);
+            function frame() {
+                if (width >= 100) {
+                    clearInterval(id);
+                    i = 0;
+                } else {
+                    width++;
+                    elem.style.width = width + "%";
+                    elem.innerHTML = width + "%";
+                }
+            }
+        }
     }
-  }
-}
