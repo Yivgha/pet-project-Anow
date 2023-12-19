c4b69576bf581a9327e6e7f23ed2e92b42d37065
quicklab <genieukne@gmail.com>
2023-12-19 20:30:40 +0200

    feat: team styles for screens

--- js/skillbar.js
+++ js/skillbar.js
@@ -1,23 +1,26 @@
-var i = 0;
+let i = 0;
+const skillbox = document.getElementById("skillbox");
 
 function move() {
-    if (i == 0) {
-        i = 1;
-        var elem = document.getElementById("myBar");
-        var elem2 = document.getElementById("myBar2");
-        var elem3 = document.getElementById("myBar3");
-        var width = 1;
-        var id = setInterval(frame, 10);
-        function frame() {
-            if (width >= 100) {
-                clearInterval(id);
-                i = 0;
-            } else {
-                width++;
-                elem.style.width = width + "%";
-                elem2.style.width = width + "%";
-                elem3.style.width = width + "%";
-            }
-        }
+  if (i == 0) {
+    i = 1;
+    let elem = document.getElementById("myBar");
+    let elem2 = document.getElementById("myBar2");
+    let elem3 = document.getElementById("myBar3");
+    let width = 1;
+    let id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem2.style.width = width + "%";
+        elem3.style.width = width + "%";
+      }
     }
+  }
 }
+
+skillbox.addEventListener("mouseover", move, { once: true });
