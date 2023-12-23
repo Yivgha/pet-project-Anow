567e030fd4a20d018dc56de9b86bbf35ea04accd
quicklab <genieukne@gmail.com>
2023-12-23 10:42:40 +0200

    feat: hamburger menu

---
+++ js/hamburgerMenu.js
@@ -0,0 +1,42 @@
+const x = document.getElementById("myLinks");
+const navLink = document.querySelector(".nav-link");
+const allMenuLinks = document.querySelectorAll(".nav-link");
+const icon = document.getElementById("toggleIcon");
+const menu = document.querySelector(".hamburger-container");
+
+function toggleMenu() {
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+    menu.style.display = "none";
+    icon.className = "fa fa-bars";
+    menu.classList.remove("open");
+  } else {
+    x.style.display = "flex";
+    menu.style.display = "flex";
+    icon.className = "fa fa-times";
+    menu.classList.add("open");
+  }
+}
+
+function closeMenu() {
+  x.style.display = "none";
+  menu.style.display = "none";
+  icon.className = "fa fa-bars";
+  menu.classList.remove("open");
+}
+
+menu.addEventListener("click", closeMenu);
+
+for (let counter = 0; counter < allMenuLinks.length; ++counter) {
+  allMenuLinks[counter].addEventListener("click", () => {
+    if (menu.classList.contains("open")) {
+      menu.classList.remove("open");
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  if (window.innerWidth < 768) {
+    icon.className = "fa fa-bars";
+  }
+});
